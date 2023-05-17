764ba4fd3199884e1862afdff6905422fc876594
Jo Kalnavarna <madara.k@hotmail.co.uk>
2023-05-17 17:15:44 +0100

    made a mess with tsc so moved it for now in practice-typescript folder but in future need to figure it out a better way

---
+++ practice-typescript/build/isAllUpper.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function isAllUpper(uppercase) {
+    return uppercase == uppercase.toUpperCase() ? true : false;
+}
+exports.default = isAllUpper;
+//# sourceMappingURL=isAllUpper.js.map
